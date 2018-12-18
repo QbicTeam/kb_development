2e9195989b7315fc8a2f781138ff30210e2dd67c
majahide <majahide.payan@hotmail.com>
2018-12-18 08:36:42 -0800

    Interface for present and Input form done

--- kb-development-SPA/src/app/app.module.ts
+++ kb-development-SPA/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { LinkInfoCardComponent } from './linkInfoCard/linkInfoCard.component';
+import { LinkinfoService } from './_services/linkinfo.service';
+import { EditLinkInfoComponent } from './editLinkInfo/editLinkInfo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavbarComponent,
+      HomeComponent,
+      LinkInfoCardComponent,
+      EditLinkInfoComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      FormsModule,
+      ReactiveFormsModule
+   ],
+   providers: [
+      LinkinfoService
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
